b6302f35333b0f6321e62d96323e6f04e1f776dc
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-05-12 23:22:35 +0200

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,195 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { Grid, Box, Button, TextField, Paper } from "@mui/material";
+import moment from "moment";
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+interface FormValues {
+  name: string;
+  preparation_time: string;
+  type: string;
+  no_of_slices: number;
+  diameter: number;
+  spiciness_scale: number;
+  slices_of_bread: number;
 }
+
+const HomePage = () => {
+  const initialValues = {
+    name: "",
+    preparation_time: "",
+    type: "",
+    no_of_slices: 1,
+    diameter: 1,
+    spiciness_scale: 1,
+    slices_of_bread: 1,
+  };
+
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().required("Required"),
+    preparation_time: Yup.string()
+      .matches(/^[0-9][0-9]:[0-5][0-9]:[0-5][0-9]$/, "Invalid time format")
+      .required("Required"),
+    type: Yup.string().required("Required"),
+    no_of_slices: Yup.number().test("is-pizza", "Invalid type", () => {
+      if (initialValues.type === "pizza") {
+        return true;
+      }
+      return false;
+    }),
+    diameter: Yup.number().test("is-pizza", "Invalid type", () => {
+      if (initialValues.type === "pizza") {
+        return true;
+      }
+      return false;
+    }),
+    spiciness_scale: Yup.number().test("is-soup", "Invalid type", () => {
+      if (initialValues.type === "soup") {
+        return true;
+      }
+      return false;
+    }),
+    slices_of_bread: Yup.number().test("is-sandwich", "Invalid type", () => {
+      if (initialValues.type === "sandwich") {
+        return true;
+      }
+      return false;
+    }),
+  });
+  const onSubmit = (values: FormValues) => {
+    const data = {
+      ...values,
+      preparation_time: moment(values.preparation_time, "HH:mm:ss").format(
+        "HH:mm:ss"
+      ),
+    };
+    console.log(data);
+  };
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit,
+  });
+  const styles = {
+    formControl: {
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: 2,
+    },
+  };
+  return (
+    <Grid container justifyContent="center">
+      <Grid item xs={12} md={6}>
+        <Paper elevation={3} sx={{ p: 4 }}>
+          <form onSubmit={formik.handleSubmit}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  id="name"
+                  name="name"
+                  label="Name"
+                  value={formik.values.name}
+                  onChange={formik.handleChange}
+                  error={formik.touched.name && Boolean(formik.errors.name)}
+                  onBlur={formik.handleBlur}
+                  helperText={formik.touched.name && formik.errors.name}
+                />
+              </Grid>
+            </Grid>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  id="preparation_time"
+                  name="preparation_time"
+                  label="Preparation time"
+                  value={formik.values.preparation_time}
+                  onChange={formik.handleChange}
+                  error={
+                    formik.touched.preparation_time &&
+                    Boolean(formik.errors.preparation_time)
+                  }
+                  onBlur={formik.handleBlur}
+                  helperText={
+                    formik.touched.preparation_time &&
+                    formik.errors.preparation_time
+                  }
+                />
+              </Grid>
+            </Grid>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  id="type"
+                  name="type"
+                  label="Type"
+                  value={formik.values.type}
+                  onChange={formik.handleChange}
+                  error={
+                    formik.touched.type && Boolean(formik.errors.type)
+                  }
+                  onBlur={formik.handleBlur}
+                  helperText={
+                    formik.touched.type && formik.errors.type
+                  }
+                />
+              </Grid>
+            </Grid>
+            {formik.values.type === "pizza" && (
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <TextField
+                    fullWidth
+                    id="no_of_slices"
+                    name="no_of_slices"
+                    label="Number of slices"
+                    value={formik.values.no_of_slices}
+                    onChange={formik.handleChange}
+                    error={
+                      formik.touched.no_of_slices &&
+                      Boolean(formik.errors.no_of_slices)
+                    }
+                    onBlur={formik.handleBlur}
+                    helperText={
+                      formik.touched.no_of_slices &&
+                      formik.errors.no_of_slices
+                    }
+                  />
+                </Grid>
+              </Grid>
+            )}
+            {formik.values.type === "pizza" && (
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <TextField
+                    fullWidth
+                    id="diameter"
+                    name="diameter"
+                    label="Diameter"
+                    value={formik.values.diameter}
+                    onChange={formik.handleChange}
+                    error={
+                      formik.touched.diameter &&
+                      Boolean(formik.errors.diameter)
+                    }
+                    onBlur={formik.handleBlur}
+                    helperText={
+                      formik.touched.diameter &&
+                      formik.errors.diameter
+                    }
+                  />
+                </Grid>
+              </Grid>
+            )}
+          </form>
+        </Paper>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default HomePage;
